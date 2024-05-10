c9cf0b01d70e5642b3c852735a2ea62a2e038429
Riya12aAa12 <103593113+Riya12aAa12@users.noreply.github.com>
2024-05-11 04:50:17 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,12 +24,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
@@ -46,7 +41,7 @@ const Footer = () => {
                     </a>
                 </div>
                 <div className="copyright">
-                <FaCopyright/> Komal 2024
+                <FaCopyright/> Riya 2024
                 </div>
             </ContentWrapper>
         </footer>
